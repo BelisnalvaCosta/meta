da18c6f11d22564f2078ab2224c16ee1fa3f7683
Belisnalva Costa <bella-costa@hotmail.com.br>
2022-11-19 01:39:45 -0300

    Security config and application properties

--- backend/src/main/java/com/devsuperior/devsmeta/DevsmetaApplication.java
+++ backend/src/main/java/com/devsuperior/devsmeta/DevsmetaApplication.java
@@ -1,13 +1,13 @@
-package com.devsuperior.dsmeta;
+package com.devsuperior.devsmeta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DsmetaApplication {
+public class DevsmetaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DsmetaApplication.class, args);
+		SpringApplication.run(DevsmetaApplication.class, args);
 	}
 
 }
