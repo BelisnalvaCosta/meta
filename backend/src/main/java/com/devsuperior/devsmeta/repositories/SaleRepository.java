f9a119133afaff5582c59a1aac8b98a74a122ceb
Belisnalva Costa <bella-costa@hotmail.com.br>
2022-11-19 12:22:37 -0300

    Repair error

--- backend/src/main/java/com/devsuperior/devsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/devsmeta/repositories/SaleRepository.java
@@ -1,5 +1,12 @@
 package com.devsuperior.devsmeta.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.devsuperior.devsmeta.entities.Sale;
+
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+	Page<Sale> findAll(Pageable pageable);
 
 }
