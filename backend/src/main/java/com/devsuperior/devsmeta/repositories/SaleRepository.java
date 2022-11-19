5e5a8619434e2a1225bf07da744a240de3bb6948
Belisnalva Costa <bella-costa@hotmail.com.br>
2022-11-19 15:18:38 -0300

    Database

--- backend/src/main/java/com/devsuperior/devsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/devsmeta/repositories/SaleRepository.java
@@ -1,12 +1,15 @@
 package com.devsuperior.devsmeta.repositories;
 
+import java.time.LocalDate;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.devsuperior.devsmeta.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
-
-	Page<Sale> findAll(Pageable pageable);
+	
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 
 }
